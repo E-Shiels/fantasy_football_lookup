a3e48b1f3524b0a5474e82bef74076ff6e4d9392
Eric Shiels <ericshiels2@gmail.com>
2018-08-15 19:30:57 +0000

    Scraping works mostly

--- lib/fantasy_football_lookup/player.rb
+++ lib/fantasy_football_lookup/player.rb
@@ -2,15 +2,19 @@ require "nokogiri"
 require "open-uri"
 require "pry"
 class Player
-attr_accessor :name, :team, :position, :bye_week, :rank, :postion_rank, :best_rank, :worst_rank, :average_rank, :average_draft_position, :adp_vs_rank
+attr_accessor :name, :team, :position, :bye_week, :rank, :position_rank, :best_rank, :worst_rank, :average_rank, :average_draft_position, :adp_vs_rank
 
 def self.scrape_players
 doc = Nokogiri::HTML(open('https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'))
 players = []
 doc.css("tr.player-row").each do |player_row|
+  player = self.new
   tds = [] 
     player_row.css("td").each do |td|
+      binding.pry
       tds << td
+      
+    end
       player.name = "TESTING"
       player.team = "TESTING"
       player.position = "TESTING"
@@ -22,10 +26,9 @@ doc.css("tr.player-row").each do |player_row|
       player.average_rank = tds[7].text
       player.average_draft_position = tds[9].text
       player.adp_vs_rank = tds[10].text
-      players << 
-    end
-
+    players << player
 end
+players
 end
 
 def self.find_player(player_search_input)
