d22174e3e0c2100f1f1be50c9086feea2b74327f
Eric Shiels <ericshiels2@gmail.com>
2018-08-15 18:55:37 +0000

    More work on scraping

--- lib/fantasy_football_lookup/player.rb
+++ lib/fantasy_football_lookup/player.rb
@@ -6,13 +6,25 @@ attr_accessor :name, :team, :position, :bye_week, :rank, :postion_rank, :best_ra
 
 def self.scrape_players
 doc = Nokogiri::HTML(open('https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'))
-binding.pry
 players = []
 doc.css("tr.player-row").each do |player_row|
   tds = [] 
-    player_row.css("td").each |td|
+    player_row.css("td").each do |td|
       tds << td
+      player.name = "TESTING"
+      player.team = "TESTING"
+      player.position = "TESTING"
+      player.bye_week = tds[4].text
+      player.rank = "TESTING"
+      player.position_rank = tds[3].text 
+      player.best_rank = tds[5].text
+      player.worst_rank = tds[6].text
+      player.average_rank = tds[7].text
+      player.average_draft_position = tds[9].text
+      player.adp_vs_rank = tds[10].text
+      players << 
     end
+
 end
 end
 
