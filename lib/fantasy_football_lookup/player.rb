a9ab3871eb0f440b99053e12c58f3e508e540fcb
Eric Shiels <ericshiels2@gmail.com>
2018-08-15 16:06:43 +0000

    changes that I will likely change back later

--- lib/fantasy_football_lookup/player.rb
+++ lib/fantasy_football_lookup/player.rb
@@ -1,8 +1,14 @@
+require "nokogiri"
+require "open-uri"
 class Player
 attr_accessor :name, :team, :position, :bye_week, :rank, :postion_rank, :best_rank, :worst_rank, :average_rank, :average_draft_position, :adp_vs_rank
 
 def self.scrape_players
+doc = Nokogiri::HTML(open(https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php))
+binding.pry
 player = self.new
+
+
 end
 
 def self.find_player
@@ -11,4 +17,4 @@ end
 
 end
 
-
+Player.scrape_players
